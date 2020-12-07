891ddccbc42253714254c5190d588cf5a1815980
jenelledowner17 <47220881+jenelledowner17@users.noreply.github.com>
2020-12-07 11:49:14 -0500

    edits to code

--- Project5/front_end/script.js
+++ Project5/front_end/script.js
@@ -1,22 +1,26 @@
 
 
 var updateView = async (button)=>{
-  if (button.dataset.querytype == 'by_title'){
-    let query = document.getElementById("#titleQuery").value;
-    let api = `https://localhost:3000/api/by_course_code/${query}`
+  if (button.dataset.querytype == 'by_code'){
+    let query = document.querySelector("#codeQuery").value;
+     api = `http://localhost:3000/api/by_course_code/${query}`;
+}
+else if(button.dataset.querytype == 'by_title'){
+  let query = document.querySelector("#titleQuery").value;
+   api = `http://localhost:3000/api/by_title/${query}`
 }
 else if(button.dataset.querytype == 'by_instructor'){
-  let query = document.getElementById("#NameQuery").value;
-  let api = `https://localhost:3000/api/by_instructor/${query}`
+  let query = document.querySelector("#NameQuery").value;
+   api = `http://localhost:3000/api/by_instructor/${query}`
 }
 else if(button.dataset.querytype == 'by_level'){
-  let query = document.getElementById("#levelQuery").value;
-  let api = `https://localhost:3000/api/by_level/${query}`
+  let query = document.querySelector("#levelQuery").value;
+   api = `http://localhost:3000/api/by_level/${query}`
 }
 else if(button.dataset.querytype == 'combined_query'){
-  let querylevel = document.getElementById("#levelQuery").value;
-  let queryname = document.getElementById("#InNameQuery").value;
-  let api = `https://localhost:3000/api/combined_query/${querylevel}/${queryname}`
+  let querylevel = document.querySelector("#levelQuery").value;
+  let queryname = document.querySelector("#InNameQuery").value;
+   api = `http://localhost:3000/api/combined_query/${querylevel}/${queryname}`
 }
   const data = await fetch (api);
   const model = await data.json();
@@ -27,6 +31,6 @@ var render_view =(model) =>{
   var source = document.querySelector("#display_query_view").innerHTML;
   var template = Handlebars.compile(source);
   var html = template(model);
-  document.querySelector("#display_courses").innerHTML= html;
-  console.log('view is being rendered');
+  document.querySelector("#display").innerHTML= html;
+  console.log('view has been rendered');
 }
